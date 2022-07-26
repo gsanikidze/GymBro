28490aaa14117afb502e4ac6a8a44745be15fad4
gsanikidze <sanikidze.guram@gmail.com>
2022-07-26 12:41:25 +0400

    [s-up]

--- GymBro/Utils/Constants.swift
+++ GymBro/Utils/Constants.swift
@@ -8,11 +8,21 @@
 import Foundation
 
 struct Constants {
+    // MARK: - Exercise api constants
     static let EXERCISE_API_KEY = Bundle.main.object(forInfoDictionaryKey: "EXERCISE_RAPID_API_KEY") as! String
     static let EXERCISE_API_BASE_URL = "https://exercisedb.p.rapidapi.com/exercises"
     
     static let EXERCISE_API_HEADERS = [
         "X-RapidAPI-Key": Constants.EXERCISE_API_KEY,
         "X-RapidAPI-Host": "exercisedb.p.rapidapi.com"
+    ]
+    
+    // MARK: - Youtube api constants
+    static let YOUTUBE_API_KEY = Bundle.main.object(forInfoDictionaryKey: "YOUTUBE_RAPID_API_KEY") as! String
+    static let YOUTUBE_API_BASE_URL = "https://youtube-v31.p.rapidapi.com"
+    
+    static let YOUTUBE_API_HEADERS = [
+        "X-RapidAPI-Key": Constants.YOUTUBE_API_KEY,
+        "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com"
     ]
 }
